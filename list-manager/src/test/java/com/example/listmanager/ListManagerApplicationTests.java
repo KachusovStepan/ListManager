86723e2ad93286093a1bd8f7e155df1145bb3e03
Eugene <emr201000@gmail.com>
2021-09-28 20:59:36 +0500

    first task completed!!!

---
+++ list-manager/src/test/java/com/example/listmanager/ListManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.listmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
